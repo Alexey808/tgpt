b201c13e63e3ba1baede42ce33b00ee98027d9a9
alexey.sv <alexey808sv@gmail.com>
2024-03-24 23:09:17 +0400

    fix version

--- src/openai-helper.js
+++ src/openai-helper.js
@@ -4,6 +4,7 @@ export function getErrorMessage(e) {
       errorMessage: 'Превышен лимит запросов в единицу времени'
     }
   } else {
+    console.log('Непредвиденная ошибка: ', e);
     return {
       errorMessage: 'Непредвиденная ошибка'
     }
